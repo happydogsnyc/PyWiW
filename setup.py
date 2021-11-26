0f4d97a2bdf9e649a0aae5a9b6fe4a4170ba0098
Bakr <b.annour@stuart.com>
2021-11-26 17:20:09 +0000

    no message

--- setup.py
+++ setup.py
@@ -16,15 +16,13 @@ setup(
     download_url='https://github.com/bakr-a/PyWiW/archive/refs/tags/v0.1.1.tar.gz',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',  
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Topic :: Office/Business :: Scheduling'
     ],
     packages=find_packages(),
     install_requires=[
-    'requests==2.23.0', 
-    'vcrpy==4.0.2', 
-    'pytest==5.4.2'
-    ],
+        'requests==2.23.0'
+    ]
 )
